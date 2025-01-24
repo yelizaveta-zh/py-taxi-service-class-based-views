ad41510fdc34db541d36f165fb277404c43e3fe6
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-01-24 14:40:23 +0200

    temperory_solution

--- taxi/views.py
+++ taxi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from taxi.models import Driver, Car, Manufacturer
 
+from django.views.generic import ListView
+
 
 def index(request):
     """View function for the home page of the site."""
@@ -13,3 +15,29 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").all()
+    paginate_by = 5
+
+
+class CarDetailView(ListView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(ListView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("car_set__manufacturer")
